13408de1c8df19d6031b0c52a6447035049a5bd2
kknevitt <knev123@hotmail.com>
2015-01-20 14:04:39 +0000

    added to words implementation

--- number_to_words.rb
+++ number_to_words.rb
@@ -1,5 +1,51 @@
 module NumberToWords
   def to_words(number = self)
+    case number
+    when 0
+      "zero"
+    when 1
+      "one"
+    when 2
+      "two"
+    when 3
+      "three"
+    when 4
+      "four"
+    when 5
+      "five"
+    when 6
+      "six"
+    when 7
+      "seven"
+    when 8
+      "eight"
+    when 9
+      "nine"
+    when 10
+      "ten"
+    when 11
+      "eleven"
+    when 12
+      "twelve"
+    when 13
+      "thirteen"
+    when 14
+      "fourteen"
+    when 15
+      "fifteen"
+    when 16
+      "sixteen"
+    when 17
+      "seventeen"
+    when 18
+      "eighteen"
+    when 19
+      "nineteen"
+    when 20
+      "twenty"
+    end
+
+
   end
 end
 
